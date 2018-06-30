60e4f7d54282e073fa5a58b6b15668fb0e62ebbc
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-06-30 13:45:19 +0300

    Add caching to service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,34 @@
+const CACHE_NAME = 'currency-converter-v1';
+const urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/js/main.js'
+];
+
 // Install service worker
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
+self.addEventListener('install', (event) => {
+  debugger;
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache) => {
+      console.log('Opened cache');
+	  return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // Activate service worker
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
