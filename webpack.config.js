1e76cfebbf5e5b92a14b7b4c08f73c8cfb2c42e0
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-07-01 09:01:04 +0300

    Add bundler rules

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		app: './js/main.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+		    {
+		    	test: /\.js$/,
+		    	loader: 'babel-loader',
+		    	query: {
+		    		presets: ['es2015']
+		    	}
+		    }
+		]
+	},
+	stats: {
+		colors: true
+	},
+	devtool: 'source-map'
+};
